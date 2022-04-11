a718d34cf7afa3487efda318590e0d55c63e5bf4
Jake Read <jake.read@cba.mit.edu>
2022-04-11 12:23:15 -0400

    sim sometimes, not always

---
+++ client/lit/graphicalElements.js
@@ -0,0 +1,32 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+// everything here should have a constructor for some virtual element...
+// and a delete fn 
+
+// graph element for vertex element... 
+export default function GraphicalContext(virtualVertex) {
+  // everything has its 
+  this.vvt = virtualVertex
+  // each element goes in dummy container (we could tag this, right?)
+  // this is kinda garbo but lit-html will handle one template per container, so here we are 
+  let cont = $(`<div>`).get(0)
+  $($('.plane').get(0)).append(cont)
+  // we have a global uuid (fn on muleClient.js) 
+  this.uuid = window.getNewElementUUID()
+  // we are a lit element 
+  let template = (self) => html`
+  <div class="vcontext" id="${self.uuid}" style="position:absolute; border: none; width: 50px; height: 50px; 
+  transform: scale(1); left: ${self.state.x}px; top: ${self.state.y}px; background-color: black;"></div>
+  `
+  // obj has state, 
+  this.state = { x: 0, y: 0 }
+  // has render call, 
+  this.render = () => {
+    render(template(this), cont)
+  }
+  // can rm thing 
+  this.delete = () => {
+    $(cont).remove()
+  }
+}
+
--- client/netrunner/netDoodler.js
+++ client/netrunner/netDoodler.js
@@ -15,15 +15,25 @@ no warranty is provided, and users accept all liability.
 'use strict'
 
 import DT from '../interface/domTools.js'
-import { VT, TIMES } from '../../core/ts.js'  
+import { VT, TIMES } from '../../core/ts.js'
 
-import {html, render} from 'https://unpkg.com/lit-html?module';
-import VVT from '../lit/litVertex.js';
+import { html, render } from 'https://unpkg.com/lit-html?module';
+import GraphicalContext from '../lit/graphicalElements.js';
+
+let vvtMatch = (a, b) => {
+  let ra = a.route
+  let rb = b.route
+  if (ra.length != rb.length) return false
+  for (let i = 0; i < ra.length; i++) {
+    if (ra[i] != rb[i]) return false
+  }
+  return true
+}
 
 export default function NetDoodler(xPlace, yPlace) {
   // basically the D3 example, 
   let plane = $('<div>').attr('id', 'my_dataviz').get(0)//.css('background-color', 'ghostwhite').get(0)
-  DT.placeField(plane, 400, 400, xPlace, yPlace)
+  DT.placeField(plane, 1000, 1000, xPlace, yPlace)
 
   // set the dimensions and margins of the graph
   const margin = { top: 10, right: 30, bottom: 30, left: 40 },
@@ -39,105 +49,134 @@ export default function NetDoodler(xPlace, yPlace) {
     .attr("transform",
       `translate(${margin.left}, ${margin.top})`);
 
-  this.redraw = (graph) => {
-    // we want to reformat the graph tree into the data layout below... 
-    // console.log('redraw', graph)
+  // previous graph, previous graphical vertices, 
+  let oldGraph = {}
+  let oldGvts = []
+
+  // first we want to diff the graph, and get a copy of it in node:links form, for D3 
+  // we get a new graph every redraw call, but have an existing copy... 
+  this.redraw = async (graph) => {
+    window.setState('drawing')
     // we'll populate these recursively... 
+    let newGvts = []
     let nodes = []; let links = []
     let lastId = 1;
     let drawTime = TIMES.getTimeStamp()
-    // graph starts at the root... we're not drawing explicitly... let's start w/ the 1st vport, 
-    let nodeRecursor = (parent, partner = undefined) => {
-      if(parent.lastDrawTime && parent.lastDrawTime == drawTime) { 
-        // console.log("bailing"); 
-        return 
+    // let's just walk the graph and try our new rendering tech, 
+    let contextRecursor = (vvt, partner = undefined) => {
+      // don't recurse back up, 
+      if (vvt.lastDrawTime && vvt.lastDrawTime == drawTime) return;
+      vvt.lastDrawTime = drawTime
+      // make a node for this thing, and an element for ourselves, 
+      let gvt = new GraphicalContext(vvt) // won't exist until we render it 
+      newGvts.push(gvt)
+      // make a simulation node, 
+      let node = { id: lastId++, name: vvt.name, index: nodes.length, vvt: vvt, gvt: gvt }
+      nodes.push(node)
+      // everything is everything 
+      gvt.node = node;
+      vvt.gvt = gvt; vvt.node = node;
+      // we have a new node, a new gvt, 
+      // if there's an element in the old gvts for this node, set fixed posn 
+      for (let gvt of oldGvts) {
+        if (vvtMatch(vvt, gvt.vvt)) {
+          console.log('found same!')
+          node.fx = gvt.state.x
+          node.fy = gvt.state.y
+        }
       }
-      // mark for no-backtracking:
-      parent.lastDrawTime = drawTime
-      // make a node: recursor should only tap each, once 
-      let node = { id: lastId ++, name: parent.name, index: nodes.length, vvt: parent }
-      nodes.push(node) 
-      // that's linked to wherever we came from, so long as it exists 
-      if(partner) links.push({source: partner, target: node})
-      // look for vports, 
-      for(let c = 0; c < parent.children.length; c ++){
-        if(parent.children[c].type == VT.VPORT){
-          let vp = parent.children[c] 
-          if(vp.reciprocal && vp.reciprocal.type != "unreachable" ) nodeRecursor(vp.reciprocal.parent, node)
+      // add link if it exists 
+      if (partner) links.push({ source: partner, target: node })
+      // sweep thru vports 
+      for (let c = 0; c < vvt.children.length; c++) {
+        if (vvt.children[c].type == VT.VPORT) {
+          let vp = vvt.children[c]
+          if (vp.reciprocal && vp.reciprocal.type != "unreachable") contextRecursor(vp.reciprocal.parent, node)
         }
       }
     }
-    // kick it, 
-    nodeRecursor(graph)
-    // draw,
-    this.render({nodes: nodes, links: links})
+    // kick it w/ root as root... 
+    contextRecursor(graph)
+    // delete all old gvts, and reset list, 
+    for (let gvt of oldGvts) {
+      gvt.delete()
+    }
+    oldGvts = newGvts
+    // do we need to use d3 ?
+    let useSim = false 
+    for(let gvt of oldGvts){
+      if(gvt.node.fx == undefined){
+        useSim = true; break;
+      }
+    } // end check for newshit 
+    try {
+      await this.settleNodes({nodes: nodes, links: links}, useSim)
+    } catch(err) {
+      console.error(err)
+    }
+    // and finally, 
+    window.setState('idle')
+    oldGraph = graph
   } // end this.redraw 
-  //render(contextTemplate({x: 100, y: 100}), document.body)
-  // data here is the vvt straight from the sweep's mouth, is the parent node of a context... 
-  this.nodeRender = (vvt) => {
-    console.warn(vvt)
-  }
-  // will have to wipe 'em 
-  let vvts = [] 
+
   // data here is like: { nodes: [ { id: <num>, name: <string>, index: indx } ], links: [ {source: <obj in nodes list>, target: <obj in nodes list>, index: indx } ] }
-  this.render = (data) => {
-    // rm and delete old, 
-    for(let vvt of vvts){
-      vvt.delete()
-    }
-    vvts = [] 
-    // make elements, 
-    for(let node of data.nodes){
-      vvts.push(new VVT(null))
-    }
-    // Initialize the links
-    const link = svg
-      .selectAll("line")
-      .data(data.links)
-      .join("line")
-      .style("stroke", "salmon")
+  this.settleNodes = (data, settle) => {
+    return new Promise((resolve, reject) => {
+      // Initialize the links
+      const link = svg
+        .selectAll("line")
+        .data(data.links)
+        .join("line")
+        .style("stroke", "salmon")
 
-    // Initialize the nodes
-    const node = svg
-      .selectAll("circle")
-      .data(data.nodes)
-      .join("circle")
-      .attr("r", 20)
-      .style("fill", "lightsalmon")
+      // Initialize the nodes
+      const node = svg
+        .selectAll("circle")
+        .data(data.nodes)
+        .join("circle")
+        .attr("r", 20)
+        .style("fill", "lightsalmon")
 
-    // Let's list the force we wanna apply on the network
-    const simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-      .force("link", d3.forceLink()                               // This force provides links between nodes
-        .id(function (d) { return d.id; })                     // This provide  the id of a node
-        .links(data.links)                                    // and this the list of links
-      )
-      .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-      .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-      .on("tick", ticked);
+      // Let's list the force we wanna apply on the network
+      const simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+        .force("link", d3.forceLink()                               // This force provides links between nodes
+          .id(function (d) { return d.id; })                     // This provide  the id of a node
+          .links(data.links)                                    // and this the list of links
+        )
+        .force("charge", d3.forceManyBody().strength(-800))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+        //.force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+        .alphaMin(0.1)
+        .on("tick", ticked)
+        .on("end", completion);
 
-    // This function is run at each iteration of the force algorithm, updating the nodes position.
-    function ticked() {
-      for(let node in data.nodes){
-        if(!vvts[node]) continue;
-        vvts[node].state.x = data.nodes[node].x
-        vvts[node].state.y = data.nodes[node].y
-        vvts[node].render()
+      // fix node 0 to home... 
+      data.nodes[0].fx = 100; data.nodes[0].fy = 100;
+
+      // This function is run at each iteration of the force algorithm, updating the nodes position.
+      function ticked() {
+        try {
+          for (let node of data.nodes) {
+            node.gvt.state.x = node.x
+            node.gvt.state.y = node.y
+            node.gvt.render()
+          }
+
+          link
+            .attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
+
+          node
+            .attr("cx", function (d) { return d.x + 6; })
+            .attr("cy", function (d) { return d.y - 6; });
+
+          // stop after one tick / update cycle if we don't need to sim... 
+          if(!settle){ simulation.stop(); resolve() }
+        } catch (err) { simulation.stop(); reject(err) }
       }
 
-      link
-        .attr("x1", function (d) { return d.source.x; })
-        .attr("y1", function (d) { return d.source.y; })
-        .attr("x2", function (d) { return d.target.x; })
-        .attr("y2", function (d) { return d.target.y; });
-
-      node
-        .attr("cx", function (d) { return d.x + 6; })
-        .attr("cy", function (d) { return d.y - 6; });
-    }
+      function completion() { resolve() }
+    })
   }
-
-  d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_network.json").then((data) => {
-    //console.log(data)
-    //this.redraw(data)
-  });
 }
