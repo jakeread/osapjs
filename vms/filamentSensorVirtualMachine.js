62d43ef710e2b4df4506cfb502f435de87f465d3
Jake Read <jake.read@cba.mit.edu>
2022-05-31 16:15:22 -0400

    filsense vm does bundles

--- vms/filamentSensorVirtualMachine.js
+++ vms/filamentSensorVirtualMachine.js
@@ -16,12 +16,23 @@ import { PK, TS } from '../../osapjs/core/ts.js'
 
 export default function FilamentSensorVM(osap, route) {
 
-  let hallQuery = osap.query(PK.route(route).sib(1).end())
-  this.getHallReading = () => {
+  let bundleQuery = osap.query(PK.route(route).sib(2).end())
+  this.getBundle = () => {
     return new Promise((resolve, reject) => {
-      hallQuery.pull().then((data) => {
-        let reading = TS.read('float32', data, 0, true)
-        resolve (reading) 
+      bundleQuery.pull().then((data) => {
+        let bundle = {
+          diameter: TS.read('float32', data, 0, true),
+          posEstimate: TS.read('float32', data, 4, true),
+          rateEstimate: TS.read('float32', data, 8, true)
+        }
+        // now... pos & rate should be divided by the circumference * encoder ticks of the thing, 
+        // which is 2^14 bits and 19mm * PI OD, 
+        // so each 16k counts is 19*PI mm of travel, 
+        let convert = (19*Math.PI) / (2**14-1)
+        console.log(convert)
+        bundle.posEstimate *= convert
+        bundle.rateEstimate *= convert 
+        resolve(bundle) 
       }).catch((err) => { reject(err) })
     })
   }
